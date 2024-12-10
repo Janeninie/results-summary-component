e81b9191ffafa63b51c2657e59417a5540eadbb1
Jane <apiwatsupaphol@gmail.com>
2024-12-10 19:19:47 +0700

    feat: finished page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,97 @@
 import Image from "next/image";
 
+interface Activity {
+  icon: string;
+  title: string;
+  score: number;
+  color: string;
+}
+
+const activities: Activity[] = [
+  {
+    icon: "/images/icon-reaction.svg",
+    title: "Reaction",
+    score: 80,
+    color: "bg-lightRed text-lightRed",
+  },
+  {
+    icon: "/images/icon-memory.svg",
+    title: "Memory",
+    score: 92,
+    color: "bg-orangeyYellow text-orangeyYellow",
+  },
+  {
+    icon: "/images/icon-verbal.svg",
+    title: "Verbal",
+    score: 61,
+    color: "bg-greenTeal  text-greenTeal",
+  },
+  {
+    icon: "/images/icon-visual.svg",
+    title: "visual",
+    score: 72,
+    color: "bg-cobaltBlue text-cobaltBlue",
+  },
+];
+
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <main className="min-h-screen flex sm:items-center justify-center">
+      <section className="h-[452px] max-w-screen-sm w-screen rounded-3xl flex-1 sm:flex">
+        <div className="w-full sm:w-1/2 h-full p-8 flex-col flex gap-6 justify-center items-center bg-gradient-to-b sm:rounded-3xl rounded-b-3xl from-lightSlateBlue to-lightRoyalBlue">
+          <div className="text-lightLavender">Your Result</div>
+          <div className="rounded-full w-40 h-40 gap-1 justify-center bg-gradient-to-b from-violetBlue to-persianBlue flex flex-col items-center">
+            <div className="flex flex-col items-center">
+              <h1 className="text-6xl text-white font-bold align-text-bottom">
+                76
+              </h1>
+              <h1 className="text-lightSlateBlue text-base">of 100</h1>
+            </div>
+          </div>
+          <div>
+            <h1 className="text-2xl text-white ">Great</h1>
+          </div>
+          <div className="w-4/5">
+            <h1 className="text-lightLavender text-sm text-center">
+              You scored is higher than 80% of the people who have taken these
+              test.
+            </h1>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+        <div className="sm:w-1/2 w-full h-full p-8 flex flex-col gap-6 rounded-3xl shadow-custom">
+          <div>
+            <h1 className="text-darkGrayBlue font-bold">Summary</h1>
+          </div>
+          <div className="w-full flex flex-col gap-4">
+            {activities.map((activity, idx) => (
+              <div
+                key={idx}
+                className={`w-full ${activity.color}  flex justify-between items-center bg-opacity-5 p-3 rounded-lg`}
+              >
+                <div className="flex gap-4 items-center">
+                  <Image
+                    src={activity.icon}
+                    alt="icon"
+                    width={50}
+                    height={50}
+                    className="w-auto h-auto"
+                  />
+                  <h1>{activity.title}</h1>
+                </div>
+                <h1>
+                  <span className="text-darkGrayBlue font-bold">
+                    {activity.score}
+                  </span>
+                  <span className="text-darkGrayBlue opacity-40"> / 100</span>
+                </h1>
+              </div>
+            ))}
+          </div>
+          <button className="w-full transition ease-in-out bg-darkGrayBlue from-60% text-white rounded-full p-3">
+            Continue
+          </button>
+        </div>
+      </section>
+    </main>
   );
 }
